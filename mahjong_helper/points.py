5d41c2f3fabae55a04a926193ec5c80a02b08e76
rurutoria7 <nandokabeam@gmail.com>
2022-04-15 00:59:50 +0800

    rebuild modules and classes

--- mahjong_helper/points.py
+++ mahjong_helper/points.py
@@ -1,52 +1,78 @@
-from pdb import post_mortem
-
 import math
-import yi
 
-def get_fu (splited_tiles, conditions):
-    if yi.is_qi_dui_zi():
-        return 25
-    if  yi.is_ping_he():
-        if conditions.is_tsumo:
-            return 20
-        else:
-            return 30
-    
-    fu = 0
-
-    if conditions.is_tsumo or conditions.is_men_qing:
-        return 20+fu
-    else:
-        return 30+fu
+class TilesAndConditions:
+    def __init__ (self):
+        self.free_tiles = []
+        self.group_tiles = []
+        self.is_tsumo
+        self.is_ron = not self.is_tsumo
+        self.chang_fong
+        self.zi_fong
+        self.doras
+        self.is_riichi
+        self.is_double_riichi
+        self.is_ippatsu
+        self.is_chiang_gang
+        self.is_ling_shang_kai_hua
+        self.is_hai_di_lao_yue
+        self.is_tian_hu
+        self.is_di_hu
+        self.is_qin
+        self.is_zi = not self.is_qin
         
+    @property
+    def is_men_qing (self):
+        pass
 
-def get_fan (splited_tiles, conditions):
-    return 2
+class YakuChecker:
+    pass
 
-def get_basic (fu, fan):
-    fan_level = [5, 7, 10, 12]
-    fan2points = [min(fu*(2**(fan+2)),2000), 3000, 4000, 6000, 8000]
-     
-    points = 0
-    for i,f in enumerate(fan_level):
-        if fan <= f:
-            points = fan2points[i]
-            break
-    return points
+class PointsCaculator:
+    def __init__ (self, _grouped_tiles_and_cond):
+        self.grouped_tiles_and_cond = _grouped_tiles_and_cond
+        self.yaku = YakuChecker(TilesAndConditions)
+        self.fu = self.get_fu()
+        self.fan = self.get_fan()
+        self.b = self.get_basic()
 
-def ceil_to_hundred (x):
-    return (((x-1)//100)+1)*100
+    def get_fu (self):
+        if self.yaku.is_qi_dui_zi():
+            return 25
+        if  self.yaku.is_ping_he():
+            if self.grouped_tiles_and_cond.is_tsumo:
+                return 20
+            else:
+                return 30
+        fu = 0
 
-def calc_points (splited_tiles, conditions):
-    fu = get_fu(splited_tiles, conditions)
-    fan = get_fan(splited_tiles, conditions)
+        if self.grouped_tiles_and_cond.is_tsumo or self.grouped_tiles_and_cond.is_men_qing:
+            return 20+fu
+        else:
+            return 30+fu
 
-    b = get_basic(fu,fan)
-    
-    if conditions.is_qin:
-        return {"ron":ceil_to_hundred(b*6), "tsumo":ceil_to_hundred(b*2)}
-    else:
-        return {"ron":ceil_to_hundred(b*4), "tsumo":(ceil_to_hundred(b*2),ceil_to_hundred(b*1))}
+    def get_fan (self):
+        return 2
+
+    def get_basic (self):
+        fan_level = [5, 7, 10, 12]
+        fan2points = [min(self.fu*(2**(self.fan+2)),2000), 3000, 4000, 6000, 8000]
+        
+        points = 0
+        for i,f in enumerate(fan_level):
+            if self.fan <= f:
+                points = fan2points[i]
+                break
+        return points
+
+    def ceil_to_hundred (self,x):
+        return (((x-1)//100)+1)*100
+
+    def get_points (self):
+        b = self.b
+        if self.grouped_tiles_and_cond.is_qin:
+            return {"ron":self.ceil_to_hundred(b*6), "tsumo":self.ceil_to_hundred(b*2)}
+        else:
+            return {"ron":self.ceil_to_hundred(b*4), "tsumo":(self.ceil_to_hundred(b*2),self.ceil_to_hundred(b*1))}
 
 def highest_point ():
     pass
--- mahjong_helper/tiles.py
+++
@@ -1,5 +0,0 @@
-class Tiles_and_conditions:
-    pass
-
-class Yaku(Tiles_and_conditions): 
-    pass
